433c8f8f9d8de3e55caa43c39d5197751d6a4ec5
hubtwork <hubtwork@gmail.com>
2023-08-19 18:01:14 +0900

    init gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.spring.io.dependency)
+    alias(libs.plugins.kotlin.allopen) apply false
+    alias(libs.plugins.kotlin.spring) apply false
+    alias(libs.plugins.kotlin.jpa) apply false
+    id("build.logic.convention.base")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom(libs.springBoot.dependencies.get().toString())
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+
+pluginManagement {
+    includeBuild("build-logic")
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    @Suppress("UnstableApiUsage")
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+include(":api")
+include(":datasource:jpa")
+
+include(":library:monitoring")
+
+rootProject.name = "SpringBoot-Clean"
